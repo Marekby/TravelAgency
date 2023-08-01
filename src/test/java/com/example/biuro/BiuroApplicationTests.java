ef98fb708d42cdfdbbed44ed708d912b53d4b0d3
Marekby <marekby@gmail.com>
2023-08-01 20:32:44 +0200

    intial commit

---
+++ src/test/java/com/example/biuro/BiuroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biuro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiuroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
