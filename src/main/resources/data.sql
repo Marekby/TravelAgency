ff0352d2cb23179d88c08d472ed13bb084a8368f
Marekby <marekby@gmail.com>
2023-08-02 21:10:14 +0200

    data.sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,6 @@ VALUES ('Belgrade ', 'Serbia');
 
 INSERT INTO hotel (name, standard, description, city_id)
 VALUES ('Belgrade ', 3, 'test', 1);
+
+INSERT INTO hotel (name, standard, description, city_id)
+VALUES ('Riga ', 2, 'test', 2);
